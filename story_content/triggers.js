d7cf0edb1499f7a61a043c0467ec28a714908c0e
Aagam995 <aagamjain080@gmail.com>
2024-06-28 00:22:18 +0530

    set

---
+++ story_content/triggers.js
@@ -0,0 +1,22 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
+function getActor() {
+  return {
+    "mbox": "mailto:myexample@domain.com",
+    "objectType": "Agent",
+    "name": "My Example"
+  };
+}
